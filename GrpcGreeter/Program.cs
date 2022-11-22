cd7da68ec4bf74e66977a8cad88e40845ba11738
user <user@DESKTOP-5AEFLD2>
2022-11-22 17:17:59 +0800

    添加项目文件。

---
+++ GrpcGreeter/Program.cs
@@ -0,0 +1,17 @@
+using GrpcGreeter.Services;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Additional configuration is required to successfully run gRPC on macOS.
+// For instructions on how to configure Kestrel and gRPC clients on macOS, visit https://go.microsoft.com/fwlink/?linkid=2099682
+
+// Add services to the container.
+builder.Services.AddGrpc();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+app.MapGrpcService<GreeterService>();
+app.MapGet("/", () => "不允许直接访问，请使用Gprc客户端通信");
+
+app.Run();
---
+++ GrpcGreeterClient/Program.cs
@@ -0,0 +1,14 @@
+﻿using System.Threading.Tasks;
+using Grpc.Net.Client;
+using GrpcProtos;
+
+// The port number must match the port of the gRPC server.
+using var channel = GrpcChannel.ForAddress("https://localhost:7080");
+//实例化 层级 来源于proto文件内 csharp_namespace -->service名称 Greeter --> service名称 + 当前程序类型（Server | Client）
+var client = new GrpcProtos.Greeter.GreeterClient(channel);
+//调用 proto文件内  rpc服务名 SayHello --> 请求参数 为 message体 HelloRequest
+var reply = await client.SayHelloAsync(
+                  new HelloRequest { Name = "来自Gprc客户端消息" });
+Console.WriteLine("Greeting: " + reply.Message);
+Console.WriteLine("Press any key to exit...");
+Console.ReadKey();
